07850de87317396f25701c59311a200cf1e46318
Shay <shay2442@yahoo.com>
2022-06-06 18:17:37 -0500

    Can scroll through multiple images when click on Details.

--- src/components/Rooms/RoomDetails.jsx
+++ src/components/Rooms/RoomDetails.jsx
@@ -2,20 +2,38 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 const RoomDetails = () => {
   const { id } = useParams();
-  const [room, setRoom] = useState({});
+  const [room, setRoom] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
     fetch(`http://localhost:3001/rooms/${id}`)
       .then((r) => r.json())
       .then((data) => setRoom(data));
-  }, [id]);
+  }, [id, setRoom]);
 
-//   console.log("room", room);
+  if (!room) {
+    return;
+  }
 
   return (
     <div>
       {/* <img src={ place.image } alt="place picture" height="300" width="350"  /> */}
-      <h1>{room.id}</h1>
+      <button
+        disabled={currentImageIndex === 0}
+        onClick={() => setCurrentImageIndex(currentImageIndex - 1)}
+      >
+        Previous Image
+      </button>
+      <img
+        src={room.images[currentImageIndex].url}
+        style={{ height: "300px", width: "400px" }}
+      />
+      <button
+        disabled={currentImageIndex === room.images.length - 1}
+        onClick={() => setCurrentImageIndex(currentImageIndex + 1)}
+      >
+        Next Image
+      </button>
       <h3>
         {" "}
         Items Included:{" "}
