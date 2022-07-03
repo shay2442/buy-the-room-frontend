d18fd3617941332a104c15342d35c6c7a0d83345
Shay <shay2442@yahoo.com>
2022-07-03 13:27:32 -0500

    Switched baseUrl to heroku

--- src/Globals.js
+++ src/Globals.js
@@ -1,11 +1,12 @@
-export const baseUrl = 'http://localhost:3001'
+// export const baseUrl = "http://localhost:3001";
+export const baseUrl = "https://buy-the-room-backend.herokuapp.com";
 export const headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-  } 
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
 
-  export const getToken = () => {
-    return {
-      'Authorization': `bearer ${ localStorage.getItem('jwt') }`
-    }
-  }
+export const getToken = () => {
+  return {
+    Authorization: `bearer ${localStorage.getItem("jwt")}`,
+  };
+};
